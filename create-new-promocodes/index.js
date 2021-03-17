c61f29d8f03c56474853430df45610ca184ed16f
geekrohan <geekboyrohan@gmail.com>
2021-03-17 21:17:29 +0530

    added 1st function to generate promocodes

---
+++ create-new-promocodes/index.js
@@ -0,0 +1,35 @@
+const { default: axios } = require("axios");
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    let amount = req.body.amount;
+    let eventLocation = req.body.location;
+    let generatedCoupons = [];
+    let todaysDate = new Date();
+    for (let i = 0; i < amount; i++) {
+        generatedCoupons.push({
+            code: coupongenerator(),
+            expireDate: todaysDate.setDate(todaysDate.getDate() + 2),
+            createdDate: todaysDate,
+            validOnLocation: eventLocation,
+            active: true,
+            validRangeinKms: 5
+        })
+    }
+    axios.post(`https://promocode-api-73b8c-default-rtdb.firebaseio.com/promoCodes.json`, generatedCoupons)
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: generatedCoupons
+    };
+}
+
+function coupongenerator() {
+    let coupon = "";
+    let possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i <= 10; i++) {
+        coupon += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return coupon;
+}
